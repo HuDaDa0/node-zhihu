1f46e73080ba2e76d46019ce318acc38d87c0358
HuDaDa0 <1497154694@qq.com>
2020-08-27 08:02:04 +0800

    使用koa-jwt库，更换自己写的中间件

--- app/routers/users.js
+++ app/routers/users.js
@@ -1,4 +1,5 @@
-const jsonwebtoken = require('jsonwebtoken');
+// const jsonwebtoken = require('jsonwebtoken');
+const jwt = require('koa-jwt');
 const Router = require('koa-router');
 const router = new Router({ prefix: '/users' });
 
@@ -6,18 +7,19 @@ const { secret } = require('../config');
 
 const { find, findById, create, update, delete: del, login, checkOwner } = require('../controllers/users')
 
-const auth = async (ctx, next) => {
-  // ctx.request.headers 会自动的把大写改成小写 Authorization=Bearer xxxx 格式
-  const { authorization = '' } = ctx.request.header;
-  const token = authorization.replace('Bearer ', '');
-  try {
-    const user = jsonwebtoken.verify(token, secret);
-    ctx.state.user = user;  // state 通常放一些用户信息
-  } catch(err) {
-    ctx.throw(401, err.message);
-  }
-  await next();
-};
+// const auth = async (ctx, next) => {
+//   // ctx.request.headers 会自动的把大写改成小写 Authorization=Bearer xxxx 格式
+//   const { authorization = '' } = ctx.request.header;
+//   const token = authorization.replace('Bearer ', '');
+//   try {
+//     const user = jsonwebtoken.verify(token, secret);
+//     ctx.state.user = user;  // state 通常放一些用户信息
+//   } catch(err) {
+//     ctx.throw(401, err.message);
+//   }
+//   await next();
+// };
+const auth = jwt({ secret });
 
 router.get('/', find);
 
